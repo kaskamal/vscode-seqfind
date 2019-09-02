ae839e0e671e8fac671ba67dd3039c34d00f0102
kaskamal <kasra.kamal@gmail.com>
2019-09-01 23:21:50 -0700

    Refactored starting seqfind

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
-import {commands, ExtensionContext, window} from 'vscode';
-import { multiStepInput, identicalSearch, complementSearch, reverseComplementSearch } from "./model/searchManager"; 
+import {commands, ExtensionContext} from 'vscode';
+import { startSeqFind } from './seqFind';
 
 
 export function activate(context: ExtensionContext) {
@@ -8,46 +8,7 @@ export function activate(context: ExtensionContext) {
 	console.log('Activating "vscode-seqfind"... :)');
 
 	let disposable = commands.registerCommand('seqfind.search', () => {
-
-		// Options to select file type / style to complete search for
-		const options: {name: string, func: (context: ExtensionContext) => Promise<void>}[] = [
-			{
-				name: "Multistep Input Search",
-				func: multiStepInput,
-			},
-			{
-				name: "Identical Search",
-				func: identicalSearch
-			},
-			{
-				name: "Complement Search",
-				func: complementSearch
-			},
-			{
-				name: "Reverse Complement Search",
-				func: reverseComplementSearch
-			}
-		];
-
-		// Pick items from list of items
-		const quickPick = window.createQuickPick();
-		quickPick.items = options.map(obj => ({ label: obj.name}));
-
-		const editor = window.activeTextEditor;
-		if (editor !== undefined) {
-			quickPick.onDidChangeSelection(sel => {
-				if (sel[0]) {
-					const selection: {name: string, func: (context: ExtensionContext) => Promise<void>} = 
-						options.filter(obj => obj.name === sel[0].label)[0];
-					selection.func(context)
-						.catch(console.error);
-				}
-			});
-			quickPick.title = "Genome Search";
-			quickPick.placeholder = "Please select";
-			quickPick.onDidHide( () => quickPick.dispose());
-			quickPick.show();
-		}
+		startSeqFind(context);
 	});
 	context.subscriptions.push(disposable);
 }
